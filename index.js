ce13e6c2167ab6dbe59e15c44bda9cbb9fff8fe4
omer <you@example.com>
2022-11-16 14:00:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let multiply;
+let num1 = 31;
+let num2 = 2;
+multiply = num1*num2;
 
-// Write your code here
+let random;
+random = Math.floor(Math.random() * 10) + 2;
+
+let num3 = 14;
+let num4 = 10;
+let mod;
+mod = num3 % num4;
+
+let max;
+max = Math.max(1, 4, 10, 13, 20);
